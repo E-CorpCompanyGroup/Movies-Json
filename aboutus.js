373d03304a59f004b87c8ed73bb635fe879a572c
E-corp Company Group <116536027+E-CorpCompanyGroup@users.noreply.github.com>
2023-01-08 21:27:59 +0300

    Delete aboutus.js
    Signed-off-by: E-corp Company Group <116536027+E-CorpCompanyGroup@users.noreply.github.com>

--- aboutus.js
+++
@@ -1,7 +0,0 @@
-let AboutData='\
-<h1>About</h1>\
-<p>Movie Lander is a Online Web Appliaction that is designed by Nagami Esther Ruth And is A Product of E-Corp Company Group.\
-It is a product of 2022 and its primary goal is to deliver movies and advertising services to the people for a cheap cost.\
-Movie Lander has All types of movies catergorised from Amination to the best series in the land and most of them are free except for the case of advertising that a client can contact us directly to get a service done from our very website or App (GiG World).\
-<p>\
-'
