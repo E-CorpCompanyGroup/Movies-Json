dd0f17c2dbc4b2c767d89a021e2bc22de8421643
E-corp Company Group <116536027+E-CorpCompanyGroup@users.noreply.github.com>
2023-01-25 12:50:38 -0800

    Add files via upload
    Signed-off-by: E-corp Company Group <116536027+E-CorpCompanyGroup@users.noreply.github.com>

---
+++ login.js
@@ -0,0 +1,31 @@
+let NewsLetter='\
+<form action="#" id="sheetdb-form" Method="Post">\
+    <h1>E-corp Company Group</h1>\
+    <br>\
+    <h1>News Letter SignUp</h1>\
+    <br>\
+    <label>UserName</label>\
+    <br>\
+    <input type="text" name="UserName" placeholder="Enter Your UserName" id="UserName" required>\
+    <br><br>\
+    <label>Email</label>\
+    <br>\
+    <input type="email" name="Email" placeholder="Enter Your Email" id="Email" required>\
+    <br><br>\
+    <button onclick="Login()">Sumbit</button>\
+</form>\
+';
+function Login() {
+    let UserName=document.querySelector('#UserName');
+    let Email=document.querySelector('#Email');
+    localStorage.setItem('UserName',UserName.value);
+    localStorage.setItem('Email',Email.value);
+    LoginDisplay.style.display="none"; 
+}
+function CheckLogin() {
+    if ( localStorage.getItem('UserName',UserName.value)&&localStorage.getItem('Email',Email.value)) {
+        LoginDisplay.style.display="none"; 
+        userName.innerHTML=localStorage.getItem('UserName',UserName.value).toLocaleUpperCase();
+        userEmail.innerHTML=localStorage.getItem('Email',Email.value).toLocaleUpperCase();
+    } 
+}
